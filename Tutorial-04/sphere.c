0fdab2ccd00d64a9080569c6f485378568bb77e8
eslam <EslamAdel>
2020-04-04 14:30:20 +0200

    lightening

--- Tutorial-04/sphere.c
+++ Tutorial-04/sphere.c
@@ -1,9 +1,18 @@
 #include <GL/glut.h>
 
-float light_ambient[] = {1.0, 0.0, 0.0, 1.0};   
-float light_diffuse[] = {1.0, 0.0, 0.0, 1.0};  
-float light_specular[] = {1.0, 1.0, 1.0, 1.0}; 
-float light_position[] = {0.0, 0.0, 4.0, 1.0};
+//Setting White Light Source RGBA
+GLfloat light_ambient[] = {1.0, 0.0, 1.0, 1.0},
+        light_diffuse[] = {1.0, 0.0, 1.0, 1.0},
+        light_specular[] = {1.0, 0.0, 1.0, 1.0},
+        light_position[] = {0.0, 1.5, 4.0, 1.0};
+
+//Setting materials for Red object
+GLfloat material_ambient[] = {0.1, 0.01, 0.01, 1.0},
+        material_diffuse[] = {0.3, 0.01, 0.01, 1.0},
+        material_specular[] = {1, 0.01, 0.01, 1.0},
+        material_shininess = 30;
+
+//Setting Camera
 float eye[] = {0.0, 0.0, 5.0};
 float center[] = {0.0, 0.0, 0.0};
 float up[] = {0.0, 1.0, 0.0};
@@ -18,7 +27,16 @@ void display()
   glutSwapBuffers();
 }
 
-void init()
+void set_material_props()
+{
+
+  glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient);
+  glMaterialfv(GL_FRONT, GL_DIFFUSE, material_diffuse);
+  glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular);
+  glMaterialf(GL_FRONT, GL_SHININESS, material_shininess);
+}
+
+void set_light_props()
 {
   glLightfv(GL_LIGHT1, GL_POSITION, light_position);
   glLightfv(GL_LIGHT1, GL_AMBIENT, light_ambient);
@@ -26,9 +44,14 @@ void init()
   glLightfv(GL_LIGHT1, GL_SPECULAR, light_specular);
   glEnable(GL_LIGHT1);
   glEnable(GL_LIGHTING);
+}
+
+void init()
+{
+  set_material_props();
+  set_light_props();
 
   glEnable(GL_DEPTH_TEST);
-
   glMatrixMode(GL_PROJECTION);
   gluPerspective(100.0, 1.0, 1.0, 10.0);
   gluLookAt(eye[0], eye[1], eye[2],
@@ -40,11 +63,10 @@ void init()
 int main(int argc, char **argv)
 {
   glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
   glutCreateWindow("red 3D lighted sphere");
   glutDisplayFunc(display);
   init();
   glutMainLoop();
   return 0;
 }
-
