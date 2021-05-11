d26b7bd0412b0f9efac99a80d36fab1de0d151cb
eslam <eslam@blinkfinance.co>
2021-05-12 01:36:40 +0200

    updating texture mapping and object loading tutorials

---
+++ Tutorial-05/object-loading/main.cpp
@@ -0,0 +1,262 @@
+#include <math.h>
+#include <GL/glut.h>
+#include "glm.h"
+
+#define PI 3.14
+
+double eye[] = {0, 0, 1};
+double center[] = {0, 0, 0};
+double up[] = {0, 1, 0};
+char *modelname = "data/al.obj";
+
+GLfloat light_ambient[] = {0.0, 0.0, 0.0, 0.0};
+GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1.0};
+GLfloat light_specular[] = {1.0, 1.0, 1.0, 1.0};
+
+GLfloat light0_position[] = {0, 0 , 2.0, 1.0};
+
+void drawmodel(char *filename)
+{
+	GLMmodel *model = glmReadOBJ(filename);
+	glmUnitize(model);
+	glmFacetNormals(model);
+	glmVertexNormals(model, 90.0);
+	glmScale(model, .15);
+	glmDraw(model, GLM_SMOOTH | GLM_MATERIAL);
+}
+
+void rotatePoint(double a[], double theta, double p[])
+{
+
+	double temp[3];
+	temp[0] = p[0];
+	temp[1] = p[1];
+	temp[2] = p[2];
+
+	temp[0] = -a[2] * p[1] + a[1] * p[2];
+	temp[1] = a[2] * p[0] - a[0] * p[2];
+	temp[2] = -a[1] * p[0] + a[0] * p[1];
+
+	temp[0] *= sin(theta);
+	temp[1] *= sin(theta);
+	temp[2] *= sin(theta);
+
+	temp[0] += (1 - cos(theta)) * (a[0] * a[0] * p[0] + a[0] * a[1] * p[1] + a[0] * a[2] * p[2]);
+	temp[1] += (1 - cos(theta)) * (a[0] * a[1] * p[0] + a[1] * a[1] * p[1] + a[1] * a[2] * p[2]);
+	temp[2] += (1 - cos(theta)) * (a[0] * a[2] * p[0] + a[1] * a[2] * p[1] + a[2] * a[2] * p[2]);
+
+	temp[0] += cos(theta) * p[0];
+	temp[1] += cos(theta) * p[1];
+	temp[2] += cos(theta) * p[2];
+
+	p[0] = temp[0];
+	p[1] = temp[1];
+	p[2] = temp[2];
+}
+void normalize(double a[])
+{
+	double norm;
+	norm = a[0] * a[0] + a[1] * a[1] + a[2] * a[2];
+	norm = sqrt(norm);
+	a[0] /= norm;
+	a[1] /= norm;
+	a[2] /= norm;
+}
+
+void crossProduct(double a[], double b[], double c[])
+{
+	c[0] = a[1] * b[2] - a[2] * b[1];
+	c[1] = a[2] * b[0] - a[0] * b[2];
+	c[2] = a[0] * b[1] - a[1] * b[0];
+}
+
+void turnLeft()
+{
+	double theta = -PI / 100;
+	rotatePoint(up, theta, eye);
+}
+
+void turnRight()
+{
+	double theta = PI / 100;
+	rotatePoint(up, theta, eye);
+}
+
+void moveUp()
+{
+	double horizontal[3];
+	double look[] = {center[0] - eye[0], center[1] - eye[1], center[2] - eye[2]};
+	crossProduct(up, look, horizontal);
+	normalize(horizontal);
+	rotatePoint(horizontal, PI / 100, eye);
+	rotatePoint(horizontal, PI / 100, up);
+}
+
+void moveDown()
+{
+
+	double horizontal[3];
+	double look[] = {center[0] - eye[0], center[1] - eye[1], center[2] - eye[2]};
+	crossProduct(up, look, horizontal);
+	normalize(horizontal);
+	rotatePoint(horizontal, -PI / 100, eye);
+	rotatePoint(horizontal, -PI / 100, up);
+}
+
+void moveForward()
+{
+
+	double speed = 0.1;
+	double direction[3];
+	direction[0] = center[0] - eye[0];
+	direction[1] = center[1] - eye[1];
+	direction[2] = center[2] - eye[2];
+	normalize(direction);
+
+	eye[0] += direction[0] * speed;
+	eye[2] += direction[2] * speed;
+
+	center[0] += direction[0] * speed;
+	center[2] += direction[2] * speed;
+}
+
+void moveBack()
+{
+
+	double speed = -0.1;
+	double direction[3];
+	direction[0] = center[0] - eye[0];
+	direction[1] = center[1] - eye[1];
+	direction[2] = center[2] - eye[2];
+
+	normalize(direction);
+
+	eye[0] += direction[0] * speed;
+	eye[2] += direction[2] * speed;
+
+	center[0] += direction[0] * speed;
+	center[2] += direction[2] * speed;
+}
+
+void reset()
+{
+    double e[] = {0.0, 0.0, 1.0};
+    double c[] = {0.0, 0.0, 0.0};
+    double u[] = {0.0, 1.0, 0.0};
+    for(int i = 0; i < 3; i++)
+    {
+        eye[i] = e[i];
+        center[i] = c[i];
+        up[i] = u[i];
+    }
+}
+
+void initRendering()
+{
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+	glEnable(GL_NORMALIZE);
+	//Enable smooth shading
+	glShadeModel(GL_SMOOTH);
+	// Enable Depth buffer
+	glEnable(GL_DEPTH_TEST);
+}
+
+void screen_menu(int value)
+{
+
+	switch (value)
+	{
+	case '1':
+		modelname = "data/al.obj";
+		break;
+	case '2':
+		modelname = "data/soccerball.obj";
+		break;
+	case '3':
+		modelname = "data/rose+vase.obj";
+		break;
+	case '4':
+		modelname = "data/apple-ibook-2001.obj";
+		break;
+	}
+	reset();
+	glutPostRedisplay();
+}
+
+void display(void)
+{
+
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	// Clear Depth and Color buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	gluLookAt(eye[0], eye[1], eye[2], center[0],
+			  center[1], center[2], up[0], up[1], up[2]);
+
+	glPushMatrix();
+	glScalef(1.5, 1.5, 1.5);
+	drawmodel(modelname);
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+void specialKeys(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_LEFT: turnLeft(); break;
+	case GLUT_KEY_RIGHT: turnRight(); break;
+	case GLUT_KEY_UP: moveUp(); break;
+	case GLUT_KEY_DOWN: moveDown(); break;
+	}
+	glutPostRedisplay();
+}
+void Keyboard(unsigned char Key, int x, int y)
+{
+	switch (Key)
+	{
+	case 'f': moveForward(); break;
+	case 'b': moveBack();  break;
+	case 'r': reset();  break;
+	case 27:  exit(0); break;
+	}
+	glutPostRedisplay();
+}
+
+void attachMenu()
+{
+	glutCreateMenu(screen_menu);
+	glutAddMenuEntry("Models", 0);
+	glutAddMenuEntry("", 0);
+	glutAddMenuEntry("Al Capone", '1');
+	glutAddMenuEntry("Soccerball", '2');
+	glutAddMenuEntry("Rose", '3');
+	glutAddMenuEntry("Pc", '4');
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
+int main(int argc, char **argv)
+{
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutCreateWindow("Model");
+	initRendering();
+
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(60, 1.0, 0.1, 10);
+	attachMenu();
+	glutDisplayFunc(display);
+	glutSpecialFunc(specialKeys);
+	glutKeyboardFunc(Keyboard);
+	glutMainLoop();
+	return 0;
+}
