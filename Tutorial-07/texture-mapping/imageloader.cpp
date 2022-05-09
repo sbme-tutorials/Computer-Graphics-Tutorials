ae06912c3976414b253a2ff28b90c8cb8d1c9d24
EslamAdel <eslam.adel.mahmoud.ali@gmail.com>
2022-05-09 15:59:56 +0200

    texture and object

---
+++ Tutorial-07/texture-mapping/imageloader.cpp
@@ -0,0 +1,157 @@
+#include <assert.h>
+#include <fstream>
+
+#include "imageloader.h"
+
+using namespace std;
+
+Image::Image(char* ps, int w, int h) : pixels(ps), width(w), height(h) {
+	
+}
+
+Image::~Image() {
+	delete[] pixels;
+}
+
+namespace {
+	//Converts a four-character array to an integer, using little-endian form
+	int toInt(const char* bytes) {
+		return (int)(((unsigned char)bytes[3] << 24) |
+					 ((unsigned char)bytes[2] << 16) |
+					 ((unsigned char)bytes[1] << 8) |
+					 (unsigned char)bytes[0]);
+	}
+	
+	//Converts a two-character array to a short, using little-endian form
+	short toShort(const char* bytes) {
+		return (short)(((unsigned char)bytes[1] << 8) |
+					   (unsigned char)bytes[0]);
+	}
+	
+	//Reads the next four bytes as an integer, using little-endian form
+	int readInt(ifstream &input) {
+		char buffer[4];
+		input.read(buffer, 4);
+		return toInt(buffer);
+	}
+	
+	//Reads the next two bytes as a short, using little-endian form
+	short readShort(ifstream &input) {
+		char buffer[2];
+		input.read(buffer, 2);
+		return toShort(buffer);
+	}
+	
+	//Just like auto_ptr, but for arrays
+	template<class T>
+	class auto_array {
+		private:
+			T* array;
+			mutable bool isReleased;
+		public:
+			explicit auto_array(T* array_ = NULL) :
+				array(array_), isReleased(false) {
+			}
+			
+			auto_array(const auto_array<T> &aarray) {
+				array = aarray.array;
+				isReleased = aarray.isReleased;
+				aarray.isReleased = true;
+			}
+			
+			~auto_array() {
+				if (!isReleased && array != NULL) {
+					delete[] array;
+				}
+			}
+			
+			T* get() const {
+				return array;
+			}
+			
+			T &operator*() const {
+				return *array;
+			}
+			
+			void operator=(const auto_array<T> &aarray) {
+				if (!isReleased && array != NULL) {
+					delete[] array;
+				}
+				array = aarray.array;
+				isReleased = aarray.isReleased;
+				aarray.isReleased = true;
+			}
+			
+			T* operator->() const {
+				return array;
+			}
+			
+			T* release() {
+				isReleased = true;
+				return array;
+			}
+			
+			void reset(T* array_ = NULL) {
+				if (!isReleased && array != NULL) {
+					delete[] array;
+				}
+				array = array_;
+			}
+			
+			T* operator+(int i) {
+				return array + i;
+			}
+			
+			T &operator[](int i) {
+				return array[i];
+			}
+	};
+}
+
+Image* loadBMP(const char* filename) {
+	ifstream input;
+	input.open(filename, ifstream::binary);
+	assert(!input.fail() || !"Could not find file");
+	char buffer[2];
+	input.read(buffer, 2);
+    input.ignore(8);
+	int dataOffset = readInt(input);
+	
+	//Read the header
+	int headerSize = readInt(input);
+	int width;
+	int height;
+	width = readInt(input);
+			height = readInt(input);
+			input.ignore(2);
+	
+	//Read the data
+	int bytesPerRow = ((width * 3 + 3) / 4) * 4 - (width * 3 % 4);
+	int size = bytesPerRow * height;
+	auto_array<char> pixels(new char[size]);
+	input.seekg(dataOffset, ios_base::beg);
+	input.read(pixels.get(), size);
+	
+	//Get the data into the right format
+	auto_array<char> pixels2(new char[width * height * 3]);
+	for(int y = 0; y < height; y++) {
+		for(int x = 0; x < width; x++) {
+			for(int c = 0; c < 3; c++) {
+				pixels2[3 * (width * y + x) + c] =
+					pixels[bytesPerRow * y + 3 * x + (2 - c)];
+			}
+		}
+	}
+	
+	input.close();
+	return new Image(pixels2.release(), width, height);
+}
+
+
+
+
+
+
+
+
+
